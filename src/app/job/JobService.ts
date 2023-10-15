9c86dd2b64cc942d6e6343a8b4b44b49addec77c
syus13 <76135546+syus13@users.noreply.github.com>
2023-10-15 02:28:54 -0300

    update

--- src/app/job/JobService.ts
+++ src/app/job/JobService.ts
@@ -2,48 +2,52 @@ import { CommonError } from "../../utils/CommonError";
 import { STATUS_CODE } from "../../utils/statusCode";
 import { TypeJob } from "./Job";
 
-class JobService{
-    constructor (
-        private repository: any,
-        private techSearchRepository: any,
-        private userRepository: any
-        ){}
+class JobService {
+  constructor(
+    private repository: any,
+    private techSearchRepository: any,
+    private userRepository: any
+  ) {}
 
-        // 
-        
-        async createJob(data: TypeJob) {
-          try {
-              return await this.repository.create(data);              
-          } catch (error: any) {
-              return CommonError.build("Error registering the job", STATUS_CODE.BAD_REQUEST);
-          }
+
+  async createJob(data: TypeJob) {
+    try {
+      return await this.repository.create(data);
+    } catch (error: any) {
+      return CommonError.build(
+        "Error registering the job",
+        STATUS_CODE.BAD_REQUEST
+      );
+    }
+  }
+
+  async filterJobs(filters: any, startIndex: number, itemsPerPage: number) {
+    try {
+      const jobAlreadyExists = await this.repository.filterJobs(
+        filters,
+        startIndex,
+        itemsPerPage
+      );
+
+      if (jobAlreadyExists.length === 0) {
+        return CommonError.build("Job not found", STATUS_CODE.NOT_FOUND);
       }
 
-        async filterJobs(userId: string, data: any) {
-            try {
-              const jobAlreadyExists = await this.repository.filterJobs(data);
-          
-              if (jobAlreadyExists.length === 0) {
-                return CommonError.build("Job not found", STATUS_CODE.NOT_FOUND);
-              }
-          
-              if (data.technology) {
-                const techCountAlready = await this.techSearchRepository.findByTech(data);
-          
-                if (techCountAlready.length === 0) {
-                  await this.techSearchRepository.createTechCount(data);
-                } else {
-                  await this.techSearchRepository.incrementTechCount(data);
-                }
-              }
-          
-              await this.userRepository.searchHistoryRecord(userId, jobAlreadyExists);
-              return jobAlreadyExists;
-            } catch (erro: any) {
-              return CommonError.build("Não foi possível realizar a pesquisa, tente novamente mais tarde", STATUS_CODE.INTERNAL_SERVER_ERROR);
-            }
-          }
-          
+      if (filters.technology) {
+        const techCountAlready = await this.techSearchRepository.upsertTechCount(
+          filters
+        );
+      }
+
+      await this.userRepository.searchHistoryRecord(filters, jobAlreadyExists);
+      return jobAlreadyExists;
+    } catch (error: any) {
+      return CommonError.build(
+        "Não foi possível realizar a pesquisa, tente novamente mais tarde",
+        STATUS_CODE.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
 }
 
-export {JobService}
+export { JobService };

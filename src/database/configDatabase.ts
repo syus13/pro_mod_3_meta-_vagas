10751bc5eeb91427bc209694f1863ffafa25c2bc
syus13 <76135546+syus13@users.noreply.github.com>
2023-10-24 19:09:58 -0300

    database

--- src/database/configDatabase.ts
+++ src/database/configDatabase.ts
@@ -5,10 +5,10 @@ import { STATUS_CODE } from "../utils/statusCode";
 class Database {
   static async initialize() {
     try {
+      await connect(process.env.DATABASE_URL as string);
       mongoose.connection.once("open", () => {
         console.log("Database ok");
       });
-      await connect(process.env.DATABASE_URL as string);
     } catch (erro: any) {
       return CommonError.build(erro.message, STATUS_CODE.INTERNAL_SERVER_ERROR)
     }
